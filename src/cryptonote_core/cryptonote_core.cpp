a3821da48daa44ec6f4592b2af7bce2ddd6bfb9e
David Ison <developer@dtison.net>
2017-12-09 23:41:49 -0500

    Renames software to electroneum in cryptonote_core.cpp.

--- src/cryptonote_core/cryptonote_core.cpp
+++ src/cryptonote_core/cryptonote_core.cpp
@@ -1290,7 +1290,7 @@ namespace cryptonote
   //-----------------------------------------------------------------------------------------------
   bool core::check_updates()
   {
-    static const char software[] = "monero";
+    static const char software[] = "electroneum";
     static const char subdir[] = "cli"; // because it can never be simple
 #ifdef BUILD_TAG
     static const char buildtag[] = BOOST_PP_STRINGIZE(BUILD_TAG);
