eb14d12f89cd224fb90ece9b7226319a2f3528d7
Andre Luis Patta Gomes <andrelg@inatel.br>
2018-03-22 18:41:18 -0300

    ASIC-Resistance changes in cryptonight algorithm

--- src/cryptonote_basic/cryptonote_format_utils.cpp
+++ src/cryptonote_basic/cryptonote_format_utils.cpp
@@ -729,7 +729,8 @@ namespace cryptonote
   bool get_block_longhash(const block& b, crypto::hash& res, uint64_t height)
   {
     blobdata bd = get_block_hashing_blob(b);
-    crypto::cn_slow_hash(bd.data(), bd.size(), res);
+    const int cn_variant = b.major_version >= 2 ? b.major_version - 1 : 0;
+    crypto::cn_slow_hash(bd.data(), bd.size(), res, cn_variant);
     return true;
   }
   //---------------------------------------------------------------
