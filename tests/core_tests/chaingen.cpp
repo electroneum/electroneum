9a9c95b61cc8960195d75f08a72c331f0b1220f9
chris <chris.harrison@electroneum.com>
2018-07-16 09:43:20 +0100

    remove comment

--- tests/core_tests/chaingen.cpp
+++ tests/core_tests/chaingen.cpp
@@ -530,7 +530,7 @@ bool construct_miner_tx_manually(size_t height, uint64_t already_generated_coins
 
   // This will work, until size of constructed block is less then CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE
   uint64_t block_reward;
-  //Deal with premine implementation. Use a dummy median > 0 so the premine scope is entered.
+  //Deal with premine implementation. 
   if (height == 1){block_reward = 1260000000000;}
   else if (!get_block_reward(0, 0, already_generated_coins, block_reward, 1))
   {
