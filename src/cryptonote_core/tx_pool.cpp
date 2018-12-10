a63ebf7ede1d39074157890275b48c7d08cf8c8d
chris <chris.harrison@electroneum.com>
2018-12-10 14:32:49 +0000

    Daemon: fix take_tx so that we can still run multiple daemons using the same database

--- src/cryptonote_core/tx_pool.cpp
+++ src/cryptonote_core/tx_pool.cpp
@@ -359,8 +359,6 @@ namespace cryptonote
     CRITICAL_REGION_LOCAL1(m_blockchain);
 
     auto sorted_it = find_tx_in_sorted_container(id);
-    if (sorted_it == m_txs_by_fee_and_receive_time.end())
-      return false;
 
     try
     {
@@ -387,6 +385,7 @@ namespace cryptonote
       return false;
     }
 
+    if (sorted_it != m_txs_by_fee_and_receive_time.end())
     m_txs_by_fee_and_receive_time.erase(sorted_it);
     return true;
   }
