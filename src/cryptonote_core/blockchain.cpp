37aa5a9676ff5952af6a1d88c9def55969dc7b18
andrepatta <andre_luis@outlook.com>
2019-01-31 19:59:46 -0200

    Removing hard codded private key used to sign blocks

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -1318,23 +1318,6 @@ bool Blockchain::create_block_template(block& b, const account_public_address& m
         ", cumulative size " << cumulative_size << " is now good");
 #endif
 
-    std::string privateKey = "-----BEGIN RSA PRIVATE KEY-----\n"\
-"MIICXAIBAAKBgQCLDy1jFeaVYt2zK0sdRq3uhGfxWWfihLLyJATlkFlrMMHE43F5\n"\
-"MbKdCtp63TdIa+2CaE5MDJx1I5hTwqs3V4IBXrg/YORGeT7aDWVfR3acpi4cX1bu\n"\
-"YlNrGUIDcoUC2sLXYes4E7XNtV5NmW6/jyJH03WfdShLVBOxkG3XlKYGVwIDAQAB\n"\
-"AoGAWTcNiWp9mw3o6pA2CrxTzpjIDwGrMpqsK8mMErP/ilOnnWfGX1jRHQvfi7/t\n"\
-"Pdtwb5/3y7azkjuZ+PMF4eWUk9ZTn8iZ/DdTlA0lQNwY3ItqEDiAJxDTr2m26JiR\n"\
-"SCf3fwSKdNx62ZQp6bUNk816yrN6jgbsZklsWEa8wbPIGPECQQC+KylB0/As1knx\n"\
-"H3MkHaUy+cC3FdVZdui2JUjtCPbSDJVVxCDHxcbcHN4AeJH7wTeRuyrZ6xNFzfgL\n"\
-"RFzLlE67AkEAuzKuwqpDanmdXe++qRD+7NN6m6xLuNpdXDGS783Mx0MOZ2xuZ/u6\n"\
-"h0ICUGDInuth7VuDar9Gj09F/TKu0KgjFQJANA50mMR/doaibxMA3d4PnsinbHbZ\n"\
-"lEfspA/IyrotdopjRrsNjiIhsZdlXhOjQps9rvLhZA3plK4xPERWcpQCiQJAcHVL\n"\
-"LaDxrSZwVn1vkcunCgkl8B16xAtS6M7TUgLVkaOEJLesQAWnEhiioTzzaR1JEDnY\n"\
-"4ikUr4VVK33mLW3E3QJBAK5AdzrKomBUWRQKFBVUhYzrOizQwSGYgxRd+em0kEFZ\n"\
-"UWyWnxFIaq0dwvr8nhaMlhp+sYLX0J69xIB3kxSPYqU=\n"\
-"-----END RSA PRIVATE KEY-----\n";
-
-
     b.dsig = crypto::encrypt_hash(get_tx_tree_hash(b), m_validator_key);
     if(b.dsig.empty()) {
       LOG_ERROR("Failed to sign block template. Please check your Validator Private Key.");
