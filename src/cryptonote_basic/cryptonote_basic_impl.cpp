c72ae7898f501a41cf7c6a682d2a25a8a8cad7c5
chris <chris.harrison@electroneum.com>
2019-02-11 10:30:35 +0000

    Reduce reward by 80% after v8

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -107,25 +107,8 @@ namespace cryptonote {
       return true;
     }
 
-    // PRE V8 ESF
-    double emission_speed_factor = EMISSION_SPEED_FACTOR_PER_MINUTE - (target_minutes-1);
-    // From V8 Taper gradient change towards that of EMISSION_SPEED_FACTOR_PER_MINUTE_V8 as a security measure.
-    // Stages should be spaced out ~ 1 month apart (21600 Blocks).
-     if(version == 8) {
-        if(!testnet){
-            if     (height < 522600) {emission_speed_factor += 0.15;}
-            else if(height < 544200) {emission_speed_factor += 0.20;}
-            else   {emission_speed_factor = EMISSION_SPEED_FACTOR_PER_MINUTE_V8 - (target_minutes-1);}
-        }
-        else if(testnet){
-            if     (height < 385600) {emission_speed_factor += 0.15;}
-            else if(height < 407200) {emission_speed_factor += 0.20;}
-            else   {emission_speed_factor = EMISSION_SPEED_FACTOR_PER_MINUTE_V8 - (target_minutes-1);}
-        }
-     }
-     else if(version > 8) {
-         emission_speed_factor = EMISSION_SPEED_FACTOR_PER_MINUTE_V8 - (target_minutes-1);
-     }
+    //After v8 the reward drops by ~80%
+    double emission_speed_factor = (version == 8 ? EMISSION_SPEED_FACTOR_PER_MINUTE_V8 : EMISSION_SPEED_FACTOR_PER_MINUTE) - (target_minutes-1);
 
     uint64_t base_reward = (MONEY_SUPPLY - already_generated_coins) / pow(2, emission_speed_factor);
 
