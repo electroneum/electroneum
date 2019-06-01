9e2a81fcc967451644a2071e49e66b859de5f2fe
Jethro Grassie <jtg@xtrabass.com>
2019-06-01 13:03:37 -0400

    add a command line option to disable ZMQ server

--- src/daemon/daemon.h
+++ src/daemon/daemon.h
@@ -47,6 +47,7 @@ private:
   uint16_t public_rpc_port;
   std::string zmq_rpc_bind_address;
   std::string zmq_rpc_bind_port;
+  bool zmq_rpc_disabled;
 public:
   t_daemon(
       boost::program_options::variables_map const & vm,
