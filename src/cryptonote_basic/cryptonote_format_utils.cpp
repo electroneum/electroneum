0825a5ed2781ac5b1d7d0d6595357f7c950b5df9
andrepatta <andre.patta@electroneum.com>
2020-04-27 15:41:32 -0300

    Removing unused functions from cryptonote_format_utils

--- src/cryptonote_basic/cryptonote_format_utils.cpp
+++ src/cryptonote_basic/cryptonote_format_utils.cpp
@@ -106,28 +106,6 @@ namespace cryptonote
   }
 }
 
-std::string hexStr(unsigned char *data, int len)
-{
-  std::stringstream ss;
-  ss << std::hex;
-  for (int i = 0; i < len; ++i)
-    ss << std::setw(2) << std::setfill('0') << (int)data[i];
-  return ss.str();
-}
-
-constexpr char hexmap[] = {'0', '1', '2', '3', '4', '5', '6', '7',
-                           '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-
-std::string hexStr2(unsigned char *data, int len)
-{
-  std::string s(len * 2, ' ');
-  for (int i = 0; i < len; ++i) {
-    s[2 * i]     = hexmap[(data[i] & 0xF0) >> 4];
-    s[2 * i + 1] = hexmap[data[i] & 0x0F];
-  }
-  return s;
-}
-
 namespace cryptonote
 {
   //---------------------------------------------------------------
