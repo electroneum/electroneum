c01f9a27ff35778d2579d962d8a0bfbb98438131
andrepatta <andre.patta@electroneum.com>
2020-04-28 11:56:34 -0300

    Update list of usable DNSSEC servers

--- src/common/dns_utils.cpp
+++ src/common/dns_utils.cpp
@@ -48,11 +48,11 @@ using namespace epee;
 
 static const char *DEFAULT_DNS_PUBLIC_ADDR[] =
 {
-  "194.150.168.168",    // CCC (Germany)
-  "81.3.27.54",         // Lightning Wire Labs (Germany)
-  "31.3.135.232",       // OpenNIC (Switzerland)
-  "80.67.169.40",       // FDN (France)
-  "209.58.179.186",     // Cyberghost (Singapore)
+    "194.150.168.168",    // CCC (Germany)
+    "80.67.169.40",       // FDN (France)
+    "89.233.43.71",       // http://censurfridns.dk (Denmark)
+    "109.69.8.51",        // punCAT (Spain)
+    "193.58.251.251"      // SkyDNS (Russia)
 };
 
 static boost::mutex instance_lock;
